c68cc357c171515817669183f576e1e919e5a3b9
Miab0t <nycexs@gmail.com>
2017-01-10 14:46:22 -0500

    Update icecream.lua

--- icecream.lua
+++ icecream.lua
@@ -10,43 +10,115 @@ else
 end
 
 
-minetest.register_craftitem("moremachines:vanilla", {
-		description = S("Vanilla"),
-		inventory_image = "vanilla.png",
-		groups = {moremachines_vanilla = 1}
-	})
-
-
-minetest.register_craftitem("moremachines:strawberry", {
-		description = S("Strawberry"),
-		inventory_image = "strawberry.png",
-                on_use = minetest.item_eat(4),
-		groups = {moremachines_strawberry = 1}
-	})
-	
-
-minetest.register_craftitem("moremachines:mint", {
-		description = S("Mint"),
-		inventory_image = "mint.png",
-                on_use = minetest.item_eat(4),
-		groups = {moremachines_mint = 1}
-	})
-	
-
-
-
-minetest.register_craftitem("moremachines:cookie", {
-		description = S("Cookie"),
-		inventory_image = "cookie.png",
-                on_use = minetest.item_eat(4),
-		groups = {moremachines_cookies = 1}
-	})
-	
-
-
-
-minetest.register_craftitem("moremachines:icecream1", {
+food.module("icecream1", function()
+	minetest.register_craftitem(":food:icecream1", {
 		description = S("Vanilla Ice Cream"),
+		inventory_image = "food_icecream_vanilla.png",
+                on_use = food.item_eat(4),
+		groups = {food_icecream1=1}
+	})
+	food.craft({
+		output = "food:icecream1",
+		recipe = {
+			{"group:food_sugar","group:food_milk","default:ice"},
+                        {"group:food_vanilla","",""},
+                        {"","",""}
+		}
+	})
+end, true)
+
+food.module("icecream2", function()
+	minetest.register_craftitem(":food:icecream2", {
+		description = S("Chocolate Ice Cream"),
+		inventory_image = "food_icecream_chocolate.png",
+                on_use = food.item_eat(4),
+		groups = {food_icecream2=1}
+	})
+	food.craft({
+		output = "food:icecream2",
+		recipe = {
+		    {"group:food_sugar","group:food_milk","default:ice"},
+                        {"group:food_cocoa", "",""},
+                        { "", "", ""} 
+
+		}
+	})
+end, true)
+
+
+food.module("icecream3", function()
+	minetest.register_craftitem(":food:icecream3", {
+		description = S("Strawberry Ice Cream"),
+		inventory_image = "food_icecream_berry.png",
+                on_use = food.item_eat(4),
+		groups = {food_icecream3=1}
+	})
+	food.craft({
+		output = "food:icecream3",
+		recipe = {
+			{"group:food_sugar","group:food_milk","default:ice"},
+			{"group:food_berry", "",""},
+			{"","",""}
+		}
+	})
+end, true)
+
+
+food.module("berry", function()
+	minetest.register_craftitem(":food:berry", {
+		description = S("Berry"),
+		inventory_image = "food_berry.png",
+		groups = {food_berry = 1}
+	})
+	food.craft({
+		output = "food:berry",
+		recipe = {
+			{"", "default:desert_sand", ""},
+			{"default:desert_sand", "", "default:desert_sand"},
+			{"", "default:desert_sand", ""}
+		}
+	})
+end, true)
+
+
+
+food.module("icecream4", function()
+	minetest.register_craftitem(":food:icecream4", {
+		description = S("Mint Chocolate Chip Ice Cream"),
+		inventory_image = "food_icecream_mint.png",
+                on_use = food.item_eat(4),
+		groups = {food_icecream4=1}
+	})
+	food.craft({
+		output = "food:icecream4",
+		recipe = {
+			{"group:food_sugar","group:food_milk","default:ice"},
+			{"group:food_mint","group:food_cocoa",""},
+			{"","",""}
+		}
+	})
+end, true)
+
+food.module("icecream5", function()
+	minetest.register_craftitem(":food:icecream5", {
+		description = S("Cookies & Cream Ice Cream"),
+		inventory_image = "food_icecream_cookies.png",
+                on_use = food.item_eat(4),
+		groups = {food_icecream5=1}
+	})
+	food.craft({
+		output = "food:icecream5",
+		recipe = {
+			{"group:food_sugar","group:food_milk","default:ice"},
+			{"group:food_cookies","",""},
+			{"","",""}
+		}
+	})
+end, true)
+
+
+
+
 		inventory_image = "icecream_vanilla.png",
                 on_use = minetest.item_eat(4),
 		groups = {moremachines_icecream1=1}
